6a37e8ef9c42db22fcaf6189206aeba7c36dd868
kaelscion <jk_cahill@hotmail.com>
2018-10-26 18:25:07 -0400

    refactored all_chars to use the string class and added an else to coin flip for lowercasing a letter if possible under certain conditions

--- pw_gen.py
+++ pw_gen.py
@@ -1,4 +1,5 @@
-from random import randint
+import string
+from random import choice
 
 class PWGen():
     def __init__(self, pw_len=25, print_to_console=False):
@@ -9,30 +10,25 @@ class PWGen():
         if i == 2:
             return True
         return False
+    
+   
 
     def generate(self, pw_len, print_to_console):
-        characters = ['a', 'b', 'c', 'd',
-                      'e', 'f', 'g', 'h', 
-                      'i', 'j', 'k', 'l',
-                      'm', 'n', 'o', 'p',
-                      'q', 'r', 's', 't',
-                      'u', 'v', 'w', 'x',
-                      'y', 'z', '0', '1',
-                      '2', '3', '4', '5',
-                      '6', '7', '8', '9',
-                      '!', '@', '#', '$',
-                      '%', '^', '&', '*',
-                      '(', ')']
-        i=1
+        '''This password generator uses random() to select a random character
+        from a list and append it to a temporary password. For added "randomness",
+        the coin_flip function is used to randomly decide to capitalize or lowercase a 
+        character if that character is a letter'''
+        
         temp_pass = []
+        all_chars = list(string.ascii_letters +string.punctuation)
         try:
-            while i <= pw_len:
-                char_index = randint(0, len(characters)-1)
-                char = characters[char_index]
+            for i in range(all_chars):
+                char = random.choice(all_chars)
                 if self.coin_flip():
-                    char = characters[char_index].upper()
+                    char  = char.upper()
+                else:
+                    char = char.lower()
                 temp_pass.append(char)
-                i+=1
                 pw = ''.join(temp_pass)
             if print_to_console:
                 print(pw)
